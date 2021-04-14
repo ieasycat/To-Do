47ce950b22f377032e6891a016b7ba1239183542
Anton klimenka <ieasycat@hotmail.com>
2021-04-14 18:39:19 +0300

    project

--- todo/urls.py
+++ todo/urls.py
@@ -3,11 +3,11 @@ from todo.views import display_main, edit_todo, delete_todo, add_todo, up, reset
 
 urlpatterns = [
     path('', display_main, name='display_main'),
-    path('<int:todo_id>', done_todo, name='done_todo'),
+    path('done_todo/<int:todo_id>', done_todo, name='done_todo'),
     path('reset/', reset, name='reset'),
     path('up/<int:todo_pos>', up, name='up'),
     path('down/<int:todo_pos>', down, name='down'),
-    path('todo_add/', add_todo, name='add_todo'),
-    path('todo_edit/<int:todo_id>', edit_todo, name='edit_todo'),
-    path('todo_del/<int:todo_id>', delete_todo, name='delete_todo'),
+    path('add_todo', add_todo, name='add_todo'),
+    path('edit_todo/<int:todo_id>', edit_todo, name='edit_todo'),
+    path('delete_todo/<int:todo_id>', delete_todo, name='delete_todo'),
 ]
