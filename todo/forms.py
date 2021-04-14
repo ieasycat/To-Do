2ad58dd88eaea2c1a02c7c6fb884c21885e95cca
Anton klimenka <ieasycat@hotmail.com>
2021-04-14 18:06:19 +0300

    project

--- todo/forms.py
+++ todo/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from todo.models import Priority
+# from todo.models import Priority
 
 
-PRIORITY_CHOISE = ((i.id, i.name) for i in Priority.objects.all())
+# PRIORITY_CHOISE = ((i.id, i.name) for i in Priority.objects.all())
 
 
 class ToDoForm(forms.Form):
     name = forms.CharField(max_length=60)
-    priority = forms.ChoiceField(choices=PRIORITY_CHOISE)
+    # priority = forms.ChoiceField(choices=PRIORITY_CHOISE)
