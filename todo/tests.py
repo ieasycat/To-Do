7b9410701b5854c5fd6e5b5f2264bed97f269fcb
Anton klimenka <ieasycat@hotmail.com>
2021-04-15 09:25:29 +0300

    project

--- todo/tests.py
+++ todo/tests.py
@@ -15,13 +15,13 @@ class TestPage(TestCase):
         )
 
     def test_add(self):
-        todo_add = self.client.post('/todo_add/')
+        todo_add = self.client.post('/add_todo')
         self.assertEqual(todo_add.status_code, 200)
 
     def test_edit(self):
-        todo_edit = self.client.post(f'/todo_edit/{self.my_todo.id}')
+        todo_edit = self.client.post(f'/edit_todo/{self.my_todo.id}')
         self.assertEqual(todo_edit.status_code, 200)
 
     def test_del(self):
-        todo_del = self.client.post(f'/todo_edit/{self.my_todo.id}')
-        self.assertEqual(todo_del.status_code, 200)
+        todo_del = self.client.post(f'/delete_todo/{self.my_todo.id}')
+        self.assertEqual(todo_del.status_code, 302)
